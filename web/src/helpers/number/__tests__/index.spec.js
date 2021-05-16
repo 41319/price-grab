91846d5e33c2037bc472c3a8ee2f0c4b05345526
Puah Chee Hau <cheehau.puah@singtel.com>
2021-05-16 19:58:37 +0800

    u

--- web/src/helpers/number/__tests__/index.spec.js
+++ web/src/helpers/number/__tests__/index.spec.js
@@ -1,9 +1,10 @@
 import { calculateSavings } from '../'
 
 describe('test calculate savings', () => {
-    it('should toggle value between mount and unmount', () => {
+    it('should return correct percentage', () => {
       expect(calculateSavings(100, 120)).toBe(20);
       expect(calculateSavings(100, 90)).toBe(0);
+      expect(calculateSavings(100, 120.1111)).toBe(20.11);
     });
   });
   
--- web/src/helpers/number/index.js
+++ web/src/helpers/number/index.js
@@ -1,5 +1,5 @@
 export const calculateSavings = (originalPrice, comparePrice) => {
     const saving = ((comparePrice - originalPrice) / originalPrice) * 100;
-    return saving > 0 ? saving : 0
+    return saving > 0 ? saving.toFixed(2) : 0
 }
   
